d01ce71f70d17dcca92d9046569b9b8a85ae1cd0
Chrissy LeMaire <clemaire@gmail.com>
2022-07-17 06:09:53 -1000

    upd

--- main.ps1
+++ main.ps1
@@ -1,18 +1,13 @@
 param (
    [string]$TemplatePath,
    [psobject[]]$ReplaceHash,
+   [string]$ReplaceHashFilePath,
    [string]$OutputPath
 )
-<#
-   TemplatePath  = ${{  inputs.template-path }}
-   ReplaceHash   = ${{  inputs.replace-hash }}
-   OutputPath    = "${{ inputs.output-path }}"
-#>
 
 if (-not (Test-Path -Path $OutputPath)) {
    $null = New-Item -Path $OutputPath -ItemType Directory -Force
 }
-
 
 Write-Output "This and that"
 $ReplaceHash | Get-Member
