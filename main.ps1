4bd602eabddf36ef4687530ae0b697f0e1e3923b
Chrissy LeMaire <clemaire@gmail.com>
2022-07-17 08:40:57 -1000

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -18,6 +18,8 @@ if ($ReplaceHashFilePath) {
    $ReplaceHash = Invoke-Expression (Get-Content -Path $ReplaceHashFilePath -Raw)
 }
 
+Write-Output $CSSPath
+
 foreach ($hash in $ReplaceHash) {
    foreach ($item in $hash) {
       $content = $template
@@ -40,16 +42,22 @@ foreach ($hash in $ReplaceHash) {
       $image = Join-Path -Path $OutputPath -ChildPath $basename.png
       $content | Set-Content -Path $filename
 
+      Write-Output $filename
+      Write-Output $outfile
+      Write-Output $image
+      Write-Output $content
       # Perform conversion
       pandoc -f markdown -t html $filename -o $outfile --self-contained --css=$CSSPath
-      npx playwright screenshot --viewport-size=1200,630 $outfile $image
+      #npx playwright screenshot --viewport-size=1200,630 $outfile $image
       if (-not $nonopt) {
          Write-Output "Optimizign $image"
          #optipng -o7 $image *> /dev/null
       }
+      <#
+            $content | Set-Content -Path /tmp/thumbs/$name.md
+            pandoc -f markdown -t html /tmp/thumbs/$name.md -o /tmp/thumbs/$name.html --self-contained --css=gh-pandoc.css
+            npx playwright screenshot --viewport-size=1200,630 /tmp/thumbs/$name.html /tmp/thumbs/$name.png
+            #optipng -o7 /tmp/thumbs/$name.png | Out-Null
+      #>
    }
 }
-
-
-Write-Output "This and that"
-$ReplaceHash | Get-Member
