5af6098365c9c5744f3fcf0a00856fbaf2cb7e53
Chrissy LeMaire <clemaire@gmail.com>
2022-07-18 21:57:52 -1000

    Update replace-template.ps1

--- sample/replace-template.ps1
+++ sample/replace-template.ps1
@@ -2,14 +2,14 @@
     @{
         "FileName"     = "MyPic"
         "--TITLE--"    = "Sample Title"
-        "--AUTHOR--"   = "Sample body"
+        "--AUTHOR--"   = "Sample Author"
         "--SYNOPSIS--" = "This too"
         "--FOOTER--"   = "Footer sample"
     },
     @{
         "FileName"     = "My Second Pic"
         "--TITLE--"    = "Another Title"
-        "--AUTHOR--"   = "Another body"
+        "--AUTHOR--"   = "Another Author"
         "--SYNOPSIS--" = "And this too"
         "--FOOTER--"   = "Another footer sample"
     }
